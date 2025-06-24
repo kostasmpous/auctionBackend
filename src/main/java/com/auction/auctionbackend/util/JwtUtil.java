1a633cc9d6b1ef19fb9ba64f6088b6ad0a1dde3f
kosta <kostasmpous@gmail.com>
2025-06-25 01:23:47 +0300

    fixed jwt

--- src/main/java/com/auction/auctionbackend/util/JwtUtil.java
+++ src/main/java/com/auction/auctionbackend/util/JwtUtil.java
@@ -3,23 +3,23 @@ package com.auction.auctionbackend.util;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
-
+import io.jsonwebtoken.security.Keys;
+import javax.crypto.SecretKey;
 public class JwtUtil {
 
-    private static final String SECRET_KEY = System.getenv("JWT_SECRET");
-
+    static SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     public static String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 day validity
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .signWith(SignatureAlgorithm.HS256, key)
                 .compact();
     }
 
     public static String validateTokenAndGetUsername(String token) {
         return Jwts.parser()
-                .setSigningKey(SECRET_KEY)
+                .setSigningKey(key)
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
