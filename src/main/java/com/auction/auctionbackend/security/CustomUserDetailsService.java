f4a5d6ea7cce40d029832921d9e7aa41895969d5
kosta <kostasmpous@gmail.com>
2025-06-25 20:45:25 +0300

    fixed jwt

---
+++ src/main/java/com/auction/auctionbackend/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.auction.auctionbackend.security;
+
+import com.auction.auctionbackend.model.User;
+import com.auction.auctionbackend.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword()) // password can be empty if not used for login
+                .authorities("USER") // or user.getRole()
+                .build();
+    }
+
+}
