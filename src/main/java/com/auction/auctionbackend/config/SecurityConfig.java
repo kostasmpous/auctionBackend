e2e097856c0420bde3e6d6a7d6afbf8be233c336
kostasmpous <kostasmpous@gmail.com>
2025-06-28 21:52:36 +0300

    added photo retrieval api

--- src/main/java/com/auction/auctionbackend/config/SecurityConfig.java
+++ src/main/java/com/auction/auctionbackend/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/photos/auction/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/bids/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/auctions/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/categories").permitAll()
